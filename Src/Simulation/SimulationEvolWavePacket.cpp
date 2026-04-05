12c2db29dcbffacae6050fd60d898fe34270869e
F. Paiva <fpaiva1011@gmail.com>
2026-04-05 22:21:49 +0100

    TODO: Implement correct accumulation and generate wavepacket

--- Src/Simulation/SimulationEvolWavePacket.cpp
+++ Src/Simulation/SimulationEvolWavePacket.cpp
@@ -19,7 +19,7 @@ class KPM_Vector;
 
 
 template <typename T>
-Eigen::Array<T, -1, 1> build_exponential(const T t) {
+Eigen::Array<T, -1, 1> build_exponential(const double t) {
   static constexpr std::array<std::complex<double>, 4> mi_pow = {{ // (-i)^n
       { 1.0,  0.0},
       { 0.0, -1.0},
@@ -27,11 +27,11 @@ Eigen::Array<T, -1, 1> build_exponential(const T t) {
       { 0.0,  1.0}
   }};
 
-  const unsigned N_pols{t * 1.5};
+  const unsigned N_pols = std::max<unsigned>(4, static_cast<unsigned>(std::ceil(1.5 * t)));
   Eigen::Array<T, -1, 1> moments(N_pols);
 
   for (int n = 0; n < N_pols; ++n) {
-    moments(n) = static_cast<double>(n == 0 ? 1 : 2) * mi_pow[n % 4] * std::cyl_bessel_jl(n, t);
+    moments(n) = static_cast<double>(n == 0 ? 1 : 2) * mi_pow[n % 4] * std::cyl_bessel_j(n, t);
   }
 
   return moments;
@@ -71,18 +71,18 @@ void Simulation<T, D>::calc_evolwave()
 #pragma omp critical
     {
       H5::H5File *file = new H5::H5File(name, H5F_ACC_RDONLY);
-      get_hdf5<value_type>(&time, file, (char *)"/Calculation/evol_wave/Time");
-      get_hdf5<value_type>(&n_measures, file, (char *)"/Calculation/evol_wave/Measurements");
+      get_hdf5<double>(&time, file, (char *)"/Calculation/evol_wave/Time");
+      get_hdf5<int>(&n_measures, file, (char *)"/Calculation/evol_wave/Measurements");
 
       file->close();
       delete file;
     }
-    evolwave(wavepacket, t, n_measures);
+    evolwave(time, n_measures);
   }
 }
 
 template <typename T, unsigned D>
-void Simulation<T, D>::evolwave(const Eigen::ArrayXcd& wavepacket, double t, int measurements)
+void Simulation<T, D>::evolwave(double t, int measurements)
 {
   debug_message("Entered evolwave\n");
   if constexpr (is_tt<std::complex, T>::value) {
@@ -104,27 +104,27 @@ void Simulation<T, D>::evolwave(const Eigen::ArrayXcd& wavepacket, double t, int
     KPM_Vector<T, D> phi(2, *this);
     Eigen::Array<T, -1, 1> ket(r.Sized);
     Eigen::Array<T, -1, 1> bra(r.Sized);
-    Eigen::Array<T, -1, -1> results(r.Sized, measurements);
+    Eigen::Array<T, -1, -1> results(r.Sized, measurements + 1); // Store initial wavepacket
     results.setZero();
 
     h.generate_disorder();
     h.generate_twists();
+    phi.initiate_phases();
+    phi.set_index(0);
+    // phi.initiate_wave_packet2(); // TODO: Implement this
+    phi.v.col(0) *= std::sqrt(size);
+    phi.Exchange_Boundaries();
+
+    results.col(0) = phi.v.col(0);
 
     for (int i = 0; i < measurements; ++i) {
-      phi.initiate_phases();
-      phi.set_index(0);
-      // phi.initiate_wave_packet2(); // TODO: Implement this
-      phi.v.col(0) *= std::sqrt(size);
-      bra = phi.v.col(0);
       ket.setZero();
-
-      phi.Exchange_Boundaries();
       for (unsigned n = 0, N = coefs.size(); n < N; ++n) {
         phi.cheb_iteration(n);
         ket += coefs(n) * phi.v.col(phi.get_index()).array();
       }
 
-      results.col(i) = ket;
+      results.col(i + 1) = ket;
     }
 
     store_evolwave(results);
@@ -138,7 +138,7 @@ void Simulation<T, D>::store_evolwave(const Eigen::Array<T, -1, -1> &results_)
   Coordinates<std::size_t, D + 1> global(r.Lt);
   Coordinates<std::size_t, D + 1> local(r.Ld);
 #pragma omp master
-  Global.evol_wave.resize(r.Sizet, resuts_.cols());
+  Global.evol_wave.resize(r.Sizet, results_.cols());
 #pragma omp barrier
   std::array<unsigned, D> idx;
   std::array<unsigned, D> start;
